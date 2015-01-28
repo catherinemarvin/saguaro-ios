acec9ce7096aabed45fc08da874dc84158682586
Kevin Hwang <k3vinhwang@gmail.com>
2015-01-28 00:53:45 -0800

    Initial Commit

---
+++ Saguaro-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Saguaro-iOS
+//
+//  Created by Kevin Hwang on 1/28/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Saguaro-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Saguaro-iOS
+//
+//  Created by Kevin Hwang on 1/28/15.
+//  Copyright (c) 2015 Kevin Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
